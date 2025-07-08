6dc16d44503bc83793690fba1602ed8401f194c3
kaiftaufiq <kaifshaikhtaufiq@gmail.com>
2025-07-09 04:40:05 +0530

    shadcn init

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Button } from '@/components/ui/button'
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div className="min-h-screen w-full flex items-center justify-center">TanStack Start init!</div>
+  return <div className="min-h-screen w-full flex items-center justify-center"><Button>TanStack Start init!</Button></div>
 }
